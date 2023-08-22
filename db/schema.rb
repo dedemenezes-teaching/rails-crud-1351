5c6ee3cadd998025ea0115bc7274a8369aa96a2e
Andre Menezes <djdedemenezes@gmail.com>
2023-08-22 09:28:32 -0300

    add rating to restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_121443) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_122321) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating", default: 0, null: false
   end
 
 end
